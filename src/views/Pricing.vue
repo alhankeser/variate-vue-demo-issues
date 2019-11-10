5f23604b7d071f127bc1a96f7dd94bf97a079d77
alhankeser <alhankeser@gmail.com>
2019-11-09 19:30:50 -0500

    Create Pricing page

--- src/views/Pricing.vue
+++ src/views/Pricing.vue
@@ -1,7 +1,34 @@
 <template>
-    <div>
-    </div>
+    <main>
+        <header>
+            <div class="heroBackground features">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <section>
+                <div class="container">
+                    <div class="text-content">
+                        <div class="font-semibold uppercase">Pricing</div>
+                        <h1>Low, simple pricing for valuable tools</h1>
+                        <h2></h2>
+                    </div>
+                </div>
+            </section>
+        </header>
+        <section>
+            <div class="container">
+                <div class="content">
+                    <div class="side-text">
+                        <h2>FAQ 1</h2>
+                        <p>adfasfd</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 </template>
+
 
 <script>
 
